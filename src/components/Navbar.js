6bad366b556954767f53612534d8e8306333d71b
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-06 00:30:24 +0530

    Modify navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,37 @@
+
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import PublicIcon from '@material-ui/icons/Public';
 
 const useStyles = makeStyles((theme) => ({
-    navbar: {
+    root: {
         flexGrow: 1,
     },
-    navbackground: {
-        backgroundColor: '#2940d3',
-    }
-
+    title: {
+        flexGrow: 1,
+    },
 }));
 
 export default function Navbar() {
     const classes = useStyles();
 
     return (
-        <div className={classes.navbar}>
-            <AppBar position="sticky">
-                <Toolbar variant="dense" className={classes.navbackground}>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
                         <PublicIcon />
                     </IconButton>
-                    <Typography variant="h5" color="inherit">
+                    <Typography variant="h5" className={classes.title}>
                         Country Finder
                     </Typography>
+                    <Button href="/" e color="inherit">Home</Button>
                 </Toolbar>
             </AppBar>
         </div>
