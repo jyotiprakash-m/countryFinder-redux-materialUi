5cd36333867606e6eb935bf0da02afdfd6ec67f6
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-05 16:28:22 +0530

    remove the error from fetching api

--- src/components/CountryListing.js
+++ src/components/CountryListing.js
@@ -1,28 +1,74 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setCountries } from "../redux/actions/countriesAction";
 import CountryComponent from "./CountryComponent";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Grid } from '@material-ui/core'
+const useStyles = makeStyles((theme) => ({
+    inputWidth: {
+        textAlign: 'center',
+        [theme.breakpoints.down('sm')]: {
+            width: '95%',
+        },
+        [theme.breakpoints.up('md')]: {
+            width: '70%'
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: '50%'
+        },
+    },
 
+}));
 function CountryListing() {
+    const classes = useStyles();
+    const [api, setApi] = useState("")
+    const [inputValue, setInputValue] = useState("")
     const countries = useSelector((state) => state.allCountries.countries);
     const dispatch = useDispatch();
-    const fetchCountries = async () => {
+    const fetchCountries = async (api) => {
         const response = await axios
-            .get("https://restcountries.eu/rest/v2/all")
+            .get(api)
             .catch((err) => {
                 console.log("Err: ", err);
             });
-        dispatch(setCountries(response.data));
+        console.log(response)
+        if (response !== undefined) {
+            dispatch(setCountries(response.data));
+        }
     };
     useEffect(() => {
-        fetchCountries();
-    }, []);
+        if (inputValue === null || inputValue === "") {
+            setApi("https://restcountries.eu/rest/v2/all")
+        } else {
+            setApi(`https://restcountries.eu/rest/v2/name/${inputValue}`)
+        }
+        fetchCountries(api);
+    }, [api, inputValue]);
 
     console.log("Countries :", countries);
     return (
         <div>
-
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justify="center"
+            >
+                <TextField
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    label="Country"
+                    placeholder="Enter country name"
+                    className={classes.inputWidth}
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </Grid>
+            <CountryComponent />
         </div>
     )
 }
